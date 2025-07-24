938d6710e04c07be765b4c56a9d5f348ac710b9f
HadiHijazi2004 <hijazihadi610@gmail.com>
2025-07-24 13:21:44 +0200

    Update file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,25 +1,3 @@
-def deco(color: str):
-    color = {
-        "blue": "\033[94m",
-        "red": "\033[91m",
-        "green": "\033[92m]",
-        "yellow": "\033[93m",
-        "reset": "\033[0m"
-    }
-
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print(colors.get(color.lower(), ""), end="")
-            result = func(*args, **kwargs)
-            print(color["reset"], end="")
-            return result
-        return wrapper
-    return decorator
-
-
-
-
-
 
 
 class FileHandler:
@@ -49,7 +27,7 @@ class FileHandler:
 
 
     def __str__(self):
-         return f"<FileHandler: {self._filename}"
+         return f"<FileHandler: {self._filename}>"
 
 
     def __add__(self, other):
@@ -82,11 +60,24 @@ class AdvancedFileHandler(FileHandler):
         """check if the file is a .txt file"""
         return filename.endswith(".txt")
     
+
+
     @classmethod
     def from_content(cls, filename, content):
         """create a file from content and return a FileHandler instance"""
         with open(filename, 'w') as f:
             f.write(content)
         return cls(filename)
+    
+    def concat_files(self, *handlers):
+        new_filename = "multi_combined.txt"
+        with open(new_filename, "w") as f_out:
+            for handler in (self, *handlers):
+                with open(handler.filename, "r") as f:
+                    for line in f:
+                        f_out.write(line if line.endswith('\n') else line + '\n')
+        return FileHandler(new_filename)
+
+    
     
     
